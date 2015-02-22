55266bed4e0f90978f7c6031c4a333c5867f354a
KittenKicker <oKittenKickero@gmail.com>
2015-02-22 18:54:03 -0500

    added group project mp3 player

---
+++ groupproject/demo/js/myplaylist.js
@@ -0,0 +1,21 @@
+/**
+ * Created by 23rd and Walnut
+ * www.23andwalnut.com
+ * User: Saleem El-Amin
+ * Date: 6/8/11
+ * Time: 9:39 AM
+ */
+
+var myPlaylist = [
+
+    {
+        mp3:'audio/song1.mp3',
+        title:'Date 1',
+        artist:'DJ 1'
+    },
+    {
+        mp3:'audio/song2.mp3',
+        title:'Date 2',
+        artist:'DJ 2'
+    }
+];
