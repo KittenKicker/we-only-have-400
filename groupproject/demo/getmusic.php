fcd219d5a8e2e9ac85e8e8557401862de9bb4599
KittenKicker <oKittenKickero@gmail.com>
2015-02-22 19:14:46 -0500

    Added php file for ajax calls
    Updated index.html to make call and update array

---
+++ groupproject/demo/getmusic.php
@@ -0,0 +1,16 @@
+<?php
+$dir = 'audio/';
+$array = array();
+
+if(is_dir($dir))
+{
+    if($dh = opendir($dir)) !== false)
+    {
+        while(($file = readdir($dh)) !== false)
+        {
+            array_push($array, $file);    
+        }
+    }
+}
+
+echo json_encode($array);
